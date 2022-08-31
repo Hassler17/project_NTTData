c0254f6ae163c264b209b2b5a01784a0b4c7e7c8
Hassler17 <khferreira@misena.edu.co>
2022-08-31 17:10:08 -0500

    Updates

---
+++ precache-manifest.5c7bc4201dda24430fe83725c5a683bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5db813988d1eff0109f9",
+    "url": "/project_NTTData/static/css/main.0f24e989.chunk.css"
+  },
+  {
+    "revision": "5db813988d1eff0109f9",
+    "url": "/project_NTTData/static/js/main.5db81398.chunk.js"
+  },
+  {
+    "revision": "325ca37fb911515c36cd",
+    "url": "/project_NTTData/static/js/1.325ca37f.chunk.js"
+  },
+  {
+    "revision": "758d054ae5f9398cb0eb",
+    "url": "/project_NTTData/static/css/2.970abfa6.chunk.css"
+  },
+  {
+    "revision": "758d054ae5f9398cb0eb",
+    "url": "/project_NTTData/static/js/2.758d054a.chunk.js"
+  },
+  {
+    "revision": "cdf218fc206a0d206a85",
+    "url": "/project_NTTData/static/js/runtime~main.cdf218fc.js"
+  },
+  {
+    "revision": "644ebd3f33d897ee7f2734dd174515b6",
+    "url": "/project_NTTData/index.html"
+  }
+];
